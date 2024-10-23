2f1f90b906b76e6c7155f9f2556a1c4d4f245c47
lucia-garzon <lucgarzon.uni@gmail.com>
2024-10-23 18:06:16 -0400

    📝 try new menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-const showMoreBtn = document.querySelector('.show-more-btn');
-const description = document.querySelector('.description');
+const nav = document.querySelector(".nav"),
+  searchIcon = document.querySelector("#searchIcon"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
 
-showMoreBtn.addEventListener('click', () => {
-  if (description.style.maxHeight === '100px') {
-    description.style.maxHeight = 'none';
-    showMoreBtn.textContent = 'Show less';
-  } else {
-    description.style.maxHeight = '100px';
-    showMoreBtn.textContent = 'Show more';
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("uil-search", "uil-times");
   }
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
 });
